fb9ffc00b63ba7101d693d2f4858f4a9864aabef
lvallecillo <lmvallecillo@bancatlan.hn>
2015-05-15 10:43:23 -0600

    Agregado comentario

--- PruebaGit/src/prueba/git/Saludar.java
+++ PruebaGit/src/prueba/git/Saludar.java
@@ -7,8 +7,10 @@ public class Saludar {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Comentario agregado despues de subir el primer github
 		System.out.println("Hola Mundo");
-
+		
+	
 	}
 
 }
